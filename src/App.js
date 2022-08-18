361b87f58a51ad750f972ed2a109b78eae4f633b
wintaye <41543902+wintaye@users.noreply.github.com>
2022-08-18 15:36:49 -0500

    .map() method

--- src/App.js
+++ src/App.js
@@ -10,34 +10,33 @@ class App extends Component {
         super();
 
         this.state = {
-            name: {firstName: 'Wintaye', lastName: 'Gebru'},
-            company: 'ZTM'
+            monsters: [
+                {
+                    name: 'Linda'
+                },
+                {
+                    name: 'Frank'
+                },
+                {
+                    name: 'Jackie'
+                },
+                {
+                    name: 'Andrei'
+                }
+            ]
         };
     }
+
 
 
 
   render() {
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Hi, {this.state.name.firstName} {this.state.name.lastName} and I work at {this.state.company}
-            </p>
-            <button onClick={() => {
-                this.setState(
-                () => {
-                    return {
-                        name: { firstName: 'Andrea', lastName: 'Bocelli'},
-                };
-            },
-                () => {
-                    console.log(this.state);
-                }
-            );
-            }}>Change Name</button>
-          </header>
+            {
+                this.state.monsters.map((monster) => {
+                    return <h1>{monster.name}</h1>;
+                    })}
         </div>
     );
   }
