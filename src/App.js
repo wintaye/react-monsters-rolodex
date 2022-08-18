5087108d6952ca4fdad3496e4c147fec0add082b
wintaye <41543902+wintaye@users.noreply.github.com>
2022-08-18 14:58:38 -0500

    setState understanding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    //when component is built, runs constructor first, super calls underlying method from any classes your extending from
+    //this.state is always a json object
+    constructor() {
+        super();
+
+        this.state = {
+            name: {firstName: 'Wintaye', lastName: 'Gebru'},
+            company: 'ZTM'
+        };
+    }
+
+
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Hi, {this.state.name.firstName} {this.state.name.lastName} and I work at {this.state.company}
+            </p>
+            <button onClick={() => {
+                this.setState(
+                () => {
+                    return {
+                        name: { firstName: 'Andrea', lastName: 'Bocelli'},
+                };
+            },
+                () => {
+                    console.log(this.state);
+                }
+            );
+            }}>Change Name</button>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
